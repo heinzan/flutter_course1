95997ef4c55e9d4b1b8587059b0bc5ae00ec62b2
hein aung zan <haz.blueplanet@gmail.com>
2019-03-22 11:49:26 +0630

    [update]assign 2 finish

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 import '../product_manager.dart';
+import 'products_admin.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      drawer: Drawer(child: Column(
+        children: <Widget>[
+          AppBar(title: Text('Choose'),
+            automaticallyImplyLeading: false,
+          ),
+          ListTile(
+            title: Text('Manage Products'),
+            onTap: () {
+              Navigator.pushReplacement(context, MaterialPageRoute(
+                  builder: (BuildContext context) => ProductsAdmin())
+              );
+
+            },
+          ),
+        ],
+      ),),
       appBar: AppBar(
         title: Text('EasyList'),
       ),
